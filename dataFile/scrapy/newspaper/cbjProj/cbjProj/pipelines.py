0a588d4c31a03514c927d199aefc95e2759170f2
877549155@qq.com <Vmkash@Sky_2020>
2022-03-08 01:31:31 +0800

    cbj scrapy

---
+++ dataFile/scrapy/newspaper/cbjProj/cbjProj/pipelines.py
@@ -0,0 +1,24 @@
+
+from itemadapter import ItemAdapter
+import scrapy
+from scrapy.pipelines.images import FilesPipeline
+class CbjprojPipeline(FilesPipeline):
+    item  = None
+    # Overridable Interface
+    def get_media_requests(self, item, info):
+        self.item = item
+        url = item['url']
+        yield scrapy.Request(url)
+
+    def file_path(self, request, response=None, info=None, *, item=item):
+
+        url = item['url']
+        name = item['name']
+        # ../../../images/2022-03/07/03/2445A03C.pdf
+        index = url.index('images')+7
+        time = url[index:index+10].replace('/','-')
+        name = time+'_'+item['name']
+        path = '//CBJ2//{}//{}.pdf'.format(time[:-3],name)
+        print(path)
+
+        return path
